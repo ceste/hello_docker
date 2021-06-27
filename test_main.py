08f5de5d45abdda7d5b330f268a5470e77a87f90
Chandra Sutrisno Tjhong <chandra.sutrisno@gmail.com>
2021-06-27 09:05:11 +0700

    updating unit test

--- test_main.py
+++ test_main.py
@@ -7,4 +7,4 @@ def test_do_this():
     name = 'abc'
     do_this(name)
 
-    assert os.path.isfile('datamatrix_test_'+name+'.png')
+    assert os.path.isfile('/static/images/datamatrix_test_'+name+'.png')
